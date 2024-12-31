10ad0130055a1f45ec93f1f4da25d24a17b8d9ce
zen <xxs239239@gmail.com>
2024-12-31 21:02:22 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,49 @@
+let tokens = []; // fill with your tokens
+let channel_ids = []; // fill with your channel ids
+
+let msg = "javascript";
+
+async function fetchMessage(token, channel_id, message) {
+    return await
+    fetch(`https://discord.com/api/v9/channels/${channel_id}/messages`, {
+        "method": "POST",
+        "headers": {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        },
+        "body": JSON.stringify({
+            'content': message
+        })
+    });
+}
+
+async function cycleChannel(channel_id) {
+    while (true) {
+        for (let token of tokens) {
+            let response = await fetchMessage(token, channel_id, msg);
+
+            if (response.status === 429) {
+                let reset = response.headers.get('retry-after');
+                let timeMs = parseFloat(reset) * 1000;
+                await new Promise((resolve, reject) => setTimeout(resolve, timeMs));
+                console.log('%c' + response.status, 'color: #ff0000; font-size: 200px');
+            } else if (response.status === 403) {
+                console.log('%c' + response.status, 'color: #ffff00; font-size: 200px');
+            } else if (response.status === 200) {
+                console.log('%c' + response.status, 'color: #1f51ff; font-size: 200px');
+            } else if (response.status === 404) {
+                console.log('%c' + response.status, 'color: #99ff99; font-size: 200px');
+            }
+        }
+    }
+}
+
+
+async function spam() {
+    for (let channel_id of channel_ids) {
+        cycleChannel(channel_id);
+    }
+}
+
+spam();
